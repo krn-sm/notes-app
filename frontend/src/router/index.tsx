ae29fd6ed942596f2d02452fb590be80187039e9
krn-sm <kiranmanikkath20@gmail.com>
2026-08-28 10:07:05 +0530

    Added AuthLayout.tsx

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,17 +1,28 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import AppLayout from "../layouts/AppLayout"
+import AuthLayout from "../layouts/AuthLayout"
 import DashboardLayout from "../layouts/DashboardLayout"
 
 import HomePage from "../pages/HomePage"
 import LoginPage from "../pages/LoginPage"
 import NoteDetailPage from "../pages/NoteDetailPage"
 import NotFoundPage from "../pages/NotFoundPage"
-import RegisterPage from "../pages/RegisterPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />,
+      },
+    ],
+  },
+
+  {
+    path: "/home",
     element: <AppLayout />,
     children: [
       {
@@ -21,14 +32,17 @@ const router = createBrowserRouter([
             index: true,
             element: <HomePage />,
           },
+
           // {
           //   path: "favorites",
           //   element: <FavoritesPage />,
           // },
+
           // {
           //   path: "trash",
           //   element: <TrashPage />,
           // },
+
           // {
           //   path: "category/:categoryName",
           //   element: <CategoryPage />,
@@ -41,16 +55,6 @@ const router = createBrowserRouter([
         element: <NoteDetailPage />,
       },
     ],
-  },
-
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-
-  {
-    path: "/register",
-    element: <RegisterPage />,
   },
 
   {
