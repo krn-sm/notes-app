15a8729839ea0f79410b0144482a3717e6ee1bda
krn-sm <kiranmanikkath20@gmail.com>
2026-08-28 10:07:58 +0530

    Altered Sidebar with Brand

--- frontend/src/components/organisms/SideBar.tsx
+++ frontend/src/components/organisms/SideBar.tsx
@@ -10,9 +10,8 @@ import {
 import { useState } from "react"
 import { NavLink } from "react-router-dom"
 
-import logo from "../../assets/logo.png"
-
 import Button from "../atoms/Button"
+import Brand from "../molecules/Brand"
 import NavItem from "../molecules/NavItem"
 
 const categories = [
@@ -96,39 +95,8 @@ const Sidebar = () => {
       </Button>
 
       {/* Brand */}
-      <div
-        className={`
-          flex
-          items-center
-          ${collapsed ? "justify-center" : "gap-4 px-2"}
-        `}
-      >
-        <img
-          src={logo}
-          alt="Memoir"
-          className="
-            h-12
-            w-12
-            shrink-0
-            object-contain
-          "
-        />
 
-        {!collapsed && (
-          <h1
-            className="
-              font-display
-              text-[32px]
-              font-semibold
-              leading-none
-              tracking-wide
-              text-paper
-            "
-          >
-            Memoir
-          </h1>
-        )}
-      </div>
+      <Brand collapsed={collapsed} />
 
       {/* New Note */}
       <div
